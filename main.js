0a8f7179114197a66f747754f4b4c9151cce1402
ayush595 <81427649+ayush595@users.noreply.github.com>
2021-08-11 15:57:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,96 @@
+ 
+
+canvas= document.getElementById("canvass");
+ctx= canvas.getContext("2d");
+color= "brown";
+// to draw rect
+ctx.beginPath();
+ctx.strokeStyle= "black";
+ctx.lineWidth= 3;
+ctx.rect(100,100,500,300);
+ctx.stroke();
+// to draw any shape
+ctx.beginPath();
+ctx.strokeStyle= "red";
+ctx.lineWidth= 4;
+// to draw circle we use arc
+ctx.arc(200,200,45,0,2*Math.PI);
+ctx.stroke();
+// to draw any shape
+ctx.beginPath();
+ctx.strokeStyle= "red";
+ctx.lineWidth= 4;
+// to draw circle we use arc
+ctx.arc(200,200,45,0,2*Math.PI);
+ctx.stroke();
+
+// next 
+
+// to draw any shape
+ctx.beginPath();
+ctx.strokeStyle= "brown";
+ctx.lineWidth= 4;
+// to draw circle we use arc
+ctx.arc(300,200,45,0,2*Math.PI);
+ctx.stroke();
+// to draw any shape
+ctx.beginPath();
+ctx.strokeStyle= "red";
+ctx.lineWidth= 4;
+// to draw circle we use arc
+ctx.arc(200,200,45,0,2*Math.PI);
+ctx.stroke();
+
+// next
+
+// to draw any shape
+ctx.beginPath();
+ctx.strokeStyle= "pink";
+ctx.lineWidth= 4;
+// to draw circle we use arc
+ctx.arc(400,200,45,0,2*Math.PI);
+ctx.stroke();
+// to draw any shape
+ctx.beginPath();
+ctx.strokeStyle= "red";
+ctx.lineWidth= 4;
+// to draw circle we use arc
+ctx.arc(200,200,45,0,2*Math.PI);
+ctx.stroke();
+
+// next
+
+// to draw any shape
+ctx.beginPath();
+ctx.strokeStyle= "green";
+ctx.lineWidth= 4;
+// to draw circle we use arc
+ctx.arc(250,259,45,0,2*Math.PI);
+ctx.stroke();
+// to draw any shape
+ctx.beginPath();
+ctx.strokeStyle= "red";
+ctx.lineWidth= 4;
+// to draw circle we use arc
+ctx.arc(200,200,45,0,2*Math.PI);
+ctx.stroke();
+
+//  next
+
+// to draw any shape
+ctx.beginPath();
+ctx.strokeStyle= "teal";
+ctx.lineWidth= 4;
+// to draw circle we use arc
+ctx.arc(350,259,45,0,2*Math.PI);
+ctx.stroke();
+// to draw any shape
+ctx.beginPath();
+ctx.strokeStyle= "red";
+ctx.lineWidth= 4;
+// to draw circle we use arc
+ctx.arc(200,200,45,0,2*Math.PI);
+ctx.stroke();
+
+
+
